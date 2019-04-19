acbc68d15451360b730f14a66dc622aa61665716
brettdebeer <brettdebeer@gmail.com>
2019-04-19 08:23:42 +0000

    Merged PR 26: ci(Circle): Use env for environment file
    ci(Circle): Use env for environment file

--- apps/root-core/src/environments/environment.prod.ts
+++ apps/root-core/src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    firebase: {
-        apiKey: 'AIzaSyAxYiswSybDKoF1ljzr5DeQRvIyVNjCBPA',
-        authDomain: 'uap-root-core.firebaseapp.com',
-        databaseURL: 'https://uap-root-core.firebaseio.com',
-        messagingSenderId: '276570862643',
-        projectId: 'uap-root-core',
-        storageBucket: 'uap-root-core.appspot.com',
-    },
     production: true,
 };
--- apps/root-core/src/environments/environment.ts
+++ apps/root-core/src/environments/environment.ts
@@ -3,14 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    firebase: {
-        apiKey: 'AIzaSyAxYiswSybDKoF1ljzr5DeQRvIyVNjCBPA',
-        authDomain: 'uap-root-core.firebaseapp.com',
-        databaseURL: 'https://uap-root-core.firebaseio.com',
-        messagingSenderId: '276570862643',
-        projectId: 'uap-root-core',
-        storageBucket: 'uap-root-core.appspot.com',
-    },
     production: false,
 };
 
